ebe2b77690f0797980edbb2f827989d71f7b2a3c
samjwillis <samjwillis97@gmail.com>
2022-01-31 16:32:45 +1100

    Much better filtering, abstracted a way abit to make it easier to implement more filters in the future

--- api/models/helpers.go
+++ api/models/helpers.go
@@ -2,6 +2,8 @@ package models
 
 import (
 	"fmt"
+	"reflect"
+	"strconv"
 )
 
 // StringFilter is a type used to filter with strings in an SQL Query
@@ -20,6 +22,33 @@ type IntFilter struct {
 	lt int64
 	gt int64
 	eq int64
+}
+
+func (f *StringFilter) fromFilterMap(filterMap map[string][]string, param string) {
+	if val, ok := filterMap[param+".li"]; ok {
+		f.li = val
+	}
+	if val, ok := filterMap[param+".eq"]; ok {
+		f.eq = val
+	}
+}
+
+func (f *IntFilter) fromFilterMap(filterMap map[string][]string, param string) {
+	if val, ok := filterMap[param+".lt"]; ok {
+		f.lt, _ = strconv.ParseInt(val[0], 10, 64)
+	} else {
+		f.lt = -1
+	}
+	if val, ok := filterMap[param+".gt"]; ok {
+		f.gt, _ = strconv.ParseInt(val[0], 10, 64)
+	} else {
+		f.gt = -1
+	}
+	if val, ok := filterMap[param+".eq"]; ok {
+		f.eq, _ = strconv.ParseInt(val[0], 10, 64)
+	} else {
+		f.eq = -1
+	}
 }
 
 func buildStringFilterQuery(filter StringFilter, colName string) (string, bool) {
@@ -48,7 +77,7 @@ func buildStringFilterQuery(filter StringFilter, colName string) (string, bool) 
 	return stmt, true
 }
 
-func buildFloat64FilterQuery(filter IntFilter, colName string) (string, bool) {
+func buildInt64FilterQuery(filter IntFilter, colName string) (string, bool) {
 	stmt := "("
 	if filter.eq != -1 {
 		stmt += fmt.Sprintf("%s = %d", colName, filter.eq)
@@ -71,4 +100,79 @@ func buildFloat64FilterQuery(filter IntFilter, colName string) (string, bool) {
 		return "", false
 	}
 	return stmt, true
+}
+
+// buildFilterQuery takes a struct that consists of filters like StringFilter and IntFilter
+// It then generates the SQL query to apply these filters
+// requires the struct fields have a tag of "col" to be able to generate query effectively
+func buildFilterQuery(filter interface{}) string {
+	var stmt string
+	filterArr := make([]string, 0)
+
+	v := reflect.Indirect(reflect.ValueOf(filter))
+	t := v.Type()
+
+	for i := 0; i < v.NumField(); i++ {
+		fieldVal := v.Field(i)
+		col := t.Field(i).Tag.Get("col")
+
+		if col == "" {
+			continue
+		}
+
+		switch reflect.TypeOf(filter).Field(i).Type {
+		case reflect.TypeOf(StringFilter{}):
+			concreteVal, _ := fieldVal.Interface().(StringFilter)
+			if val, ok := buildStringFilterQuery(concreteVal, col); ok {
+				filterArr = append(filterArr, val)
+			}
+		case reflect.TypeOf(IntFilter{}):
+			concreteVal, _ := fieldVal.Interface().(IntFilter)
+			if val, ok := buildInt64FilterQuery(concreteVal, col); ok {
+				filterArr = append(filterArr, val)
+			}
+		}
+	}
+
+	if len(filterArr) == 0 {
+		return ""
+	}
+
+	for ndx, val := range filterArr {
+		if ndx == 0 {
+			stmt += "\nWHERE "
+		} else {
+			stmt += "\nAND "
+		}
+		stmt += val
+	}
+	return stmt
+}
+
+// ParseFilterMap converts the query parameters returned by net/http into a filter as defined in the destination
+// TODO: Finish - may not be possible easily
+func ParseFilterMap(filterMap map[string][]string, dest interface{}) {
+	v := reflect.Indirect(reflect.ValueOf(dest))
+	t := v.Type()
+
+	for i := 0; i < v.NumField(); i++ {
+		param := t.Field(i).Tag.Get("param")
+
+		if param == "" {
+			continue
+		}
+
+		switch t.Field(i).Type {
+		case reflect.TypeOf(StringFilter{}):
+			elementName := param + ".li"
+			if val, ok := filterMap[elementName]; ok {
+				fmt.Println("Found: ", elementName)
+				fmt.Println(val)
+			}
+
+		case reflect.TypeOf(IntFilter{}):
+
+		}
+	}
+
 }
--- api/models/units.go
+++ api/models/units.go
@@ -3,7 +3,6 @@ package models
 import (
 	"database/sql"
 	"fmt"
-	"strconv"
 )
 
 // Unit is the structure of the unit table in the sqlite database
@@ -55,83 +54,11 @@ func (u *Unit) ReadAll(db *sql.DB, filter UnitFilter) (*[]Unit, error) {
 func ParseUnitFilterMap(filterMap map[string][]string) UnitFilter {
 	var filter UnitFilter
 
-	if val, ok := filterMap["station_name.li"]; ok {
-		filter.StationName.li = val
-	}
-	if val, ok := filterMap["station_name.eq"]; ok {
-		filter.StationName.eq = val
-	}
-	if val, ok := filterMap["region_id.li"]; ok {
-		filter.RegionID.li = val
-	}
-	if val, ok := filterMap["region_id.eq"]; ok {
-		filter.RegionID.eq = val
-	}
-	if val, ok := filterMap["fuel_source.li"]; ok {
-		filter.FuelSource.li = val
-	}
-	if val, ok := filterMap["fuel_source.eq"]; ok {
-		filter.FuelSource.eq = val
-	}
-	if val, ok := filterMap["technology_type.li"]; ok {
-		filter.TechnologyType.li = val
-	}
-	if val, ok := filterMap["technology_type.eq"]; ok {
-		filter.TechnologyType.eq = val
-	}
-	if val, ok := filterMap["max_capacity.lt"]; ok {
-		filter.MaxCapacity.lt, _ = strconv.ParseInt(val[0], 10, 64)
-	} else {
-		filter.MaxCapacity.lt = -1
-	}
-	if val, ok := filterMap["max_capacity.gt"]; ok {
-		filter.MaxCapacity.gt, _ = strconv.ParseInt(val[0], 10, 64)
-	} else {
-		filter.MaxCapacity.gt = -1
-	}
-	if val, ok := filterMap["max_capacity.eq"]; ok {
-		filter.MaxCapacity.eq, _ = strconv.ParseInt(val[0], 10, 64)
-	} else {
-		filter.MaxCapacity.eq = -1
-	}
+	filter.StationName.fromFilterMap(filterMap, "station_name")
+	filter.RegionID.fromFilterMap(filterMap, "region_id")
+	filter.FuelSource.fromFilterMap(filterMap, "fuel_source")
+	filter.TechnologyType.fromFilterMap(filterMap, "technology_type")
+	filter.MaxCapacity.fromFilterMap(filterMap, "max_capacity")
 
 	return filter
-}
-
-// Could possible use reflect to do this on an interface
-func buildFilterQuery(filter UnitFilter) string {
-	var stmt string
-	filterArr := make([]string, 0)
-
-	// Add to a Slice, then iterate over slice at the end adding in the "AND'"
-	if val, ok := buildStringFilterQuery(filter.StationName, "station_name"); ok {
-		filterArr = append(filterArr, val)
-	}
-	if val, ok := buildStringFilterQuery(filter.RegionID, "region_id"); ok {
-		filterArr = append(filterArr, val)
-	}
-	if val, ok := buildStringFilterQuery(filter.FuelSource, "fuel_source"); ok {
-		filterArr = append(filterArr, val)
-	}
-	if val, ok := buildStringFilterQuery(filter.TechnologyType, "technology_type"); ok {
-		filterArr = append(filterArr, val)
-	}
-	if val, ok := buildFloat64FilterQuery(filter.MaxCapacity, "max_capacity"); ok {
-		filterArr = append(filterArr, val)
-	}
-
-	if len(filterArr) == 0 {
-		return ""
-	}
-
-	for ndx, val := range filterArr {
-		if ndx == 0 {
-			stmt += "\nWHERE "
-		} else {
-			stmt += "\nAND "
-		}
-		stmt += val
-	}
-
-	return stmt
 }
